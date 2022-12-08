a5af5e1778772f484d4548cd2600fc0c1f7fe3fa
karastoyanov <karastoqnov.alexandar@gmail.com>
2022-12-08 13:47:07 +0000

    project update

---
+++ prometeus/get_prometeus_data.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+import xml.etree.ElementTree as ET
+
+
+tree = ET.parse('rocket.ork')
+root = tree.getroot()
+
+#for child in root:
+#    print(child.tag, child.attrib)
+new_line = f'\n------------------------\n'
+print(new_line)
+
+
+for rocket in root.findall('rocket'):
+    rocket_name = rocket.find('name').text
+    designer_name = rocket.find('designer').text
+    print(f'Rocket Name: {rocket_name}')
+    print(f'Designer Name: {designer_name}')
+    all_configs = rocket.findall('motorconfiguration')
+    print(new_line)
+    for config in all_configs:
+        print(f'Engine Configuration: {str(config.attrib.values())[14:-3]}')
+print(new_line)
