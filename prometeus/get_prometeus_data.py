da78df54b0306f070f0b7d05f79b7654b7acc2cb
karastoyanov <karastoqnov.alexandar@gmail.com>
2022-12-12 07:54:07 +0000

    project update

--- prometeus/get_prometeus_data.py
+++ prometeus/get_prometeus_data.py
@@ -5,19 +5,20 @@ import xml.etree.ElementTree as ET
 tree = ET.parse("/home/rnduser/project-prometeus/prometeus/rocket.ork")
 root = tree.getroot()
 
-#for child in root:
-#    print(child.tag, child.attrib)
 new_line = f'\n------------------------\n'
 print(new_line)
 
 
 for rocket in root.findall('rocket'):
-    rocket_name = rocket.find('name').text
-    designer_name = rocket.find('designer').text
+    rocket_name = rocket.find('name').text # Get Rocket Name
+    designer_name = rocket.find('designer').text # Get Designer Name
     print(f'Rocket Name: {rocket_name}')
     print(f'Designer Name: {designer_name}')
-    all_configs = rocket.findall('motorconfiguration')
+
+    all_configs = rocket.findall('motorconfiguration') # Find all Engine Configuration / 'TRUE' value point at the current active config
     print(new_line)
     for config in all_configs:
         print(f'Engine Configuration: {str(config.attrib.values())[14:-3]}')
 print(new_line)
+
+
