875d0b6724bfbf271b9c63917975cfe92f4a5e70
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2022-12-16 10:37:41 +0200

    project update

--- prometeus/get_prometeus_data.py
+++ prometeus/get_prometeus_data.py
@@ -9,32 +9,33 @@ root = tree.getroot()
 
 new_line = f'\n------------------------\n'
 new_subline = f'***************************'
-print(new_line)
 
 
-for rocket in root.findall('rocket'):
-    rocket_name = rocket.find('name').text  # Get Rocket Name
-    designer_name = rocket.find('designer').text  # Get Designer Name
-    print(f'Rocket Name: {rocket_name}')
-    print(f'Designer Name: {designer_name}')
+with open("result.txt", "w") as file:
+	file.write(new_line)
+	for rocket in root.findall('rocket'):
+		rocket_name = rocket.find('name').text  # Get Rocket Name
+		designer_name = rocket.find('designer').text  # Get Designer Name
+		file.write(f'Rocket Name: {rocket_name}\n')
+		file.write(f'Designer Name: {designer_name}\n')
 
-    # Find all Engine Configuration / 'TRUE' value point at the current active config
-    all_configs = rocket.findall('motorconfiguration')
-    print(new_line)
-    for config in all_configs:
-        print(f'Engine Configuration: {str(config.attrib.values())[14:-3]}')
-    print(new_line)
+		# Find all Engine Configuration / 'TRUE' value point at the current active config
+		all_configs = rocket.findall('motorconfiguration')
+		file.write(f'{new_line}\n')
+		for config in all_configs:
+			file.write(f'Engine Configuration: {str(config.attrib.values())[14:-3]}\n')
+		file.write(f'{new_line}')
 
-stages = rocket.findall('subcomponents/stage')
-for stage in stages:
-	print(f'Number of stages: {len(stages)}')
-	print(f'Stage Name: {stage.find("name").text}')
+	stages = rocket.findall('subcomponents/stage')
+	for stage in stages:
+		file.write(f'Number of stages: {len(stages)}\n')
+		file.write(f'Stage Name: {stage.find("name").text}\n')
 
-	regx = r'[<\/a-z>]'
-	subcomponents = rocket.findall(f'subcomponents/stage/subcomponents/')
+		regx = r'[<\/a-z>]'
+		subcomponents = rocket.findall(f'subcomponents/stage/subcomponents/')
 
-	for subcomponent in subcomponents:
-			print(f'\tSubcomponent')
+		for subcomponent in subcomponents:
+			file.write(f'\tSubcomponent:\n')
 			for attribute in subcomponent.iter():
 				if not attribute.text:
 						pass
@@ -42,8 +43,21 @@ for stage in stages:
 					if "\n" not in attribute.text and attribute.text is not None:
 						if attribute.text == "Trapezoidal fin set":
 							break
-						print(f'\t{attribute.tag.capitalize()} -- {attribute.text}')
-			print(new_line)
+						file.write(f'\t\t* {attribute.tag.capitalize()} -- {attribute.text}\n')
+		
+		parts = rocket.findall(f'subcomponents/stage/subcomponents/bodytube/subcomponents')
+		for part in parts:
+			file.write(f'\n\t\t\tParts:\n')
+			for part_name in part.iter():
+				if not part_name.text:
+					pass
+				else:
+					if "\n" not in part_name.text and part_name.text is not None:
+						if part_name.tag == 'name':
+							file.write(f'\n')
+							part_name.tag = f'Part {part_name.tag}'
+						file.write(f'\t\t\t\t*{part_name.tag.capitalize()} -- {part_name.text}\n')
+		file.write(new_line)
 
 					
 
@@ -53,22 +67,22 @@ a = 5
 
 
 # stages = root.find('rocket/subcomponents') # Get all stages
-# print(f'Number of stages: {len(stages)}')
+# file.write(f'Number of stages: {len(stages)}')
 
 # for stage in stages: # Iterate through all stages
-#     print(f'Stage Name: {root.find("rocket/subcomponents/stage/name").text}')
-#     print(new_subline)
+#     file.write(f'Stage Name: {root.find("rocket/subcomponents/stage/name").text}')
+#     file.write(new_subline)
 
 #     components = root.find('rocket/subcomponents/stage/subcomponents') # Get all components
-#     print(f'\tNumber of components: {len(components)}')
+#     file.write(f'\tNumber of components: {len(components)}')
 #     for component in components: # Iterate through all components
 #         component_name = root.find(f'rocket/subcomponents/stage/subcomponents/{component.tag}/name').text
-#         print(f'\t\tComponent Name: {component_name}')
+#         file.write(f'\t\tComponent Name: {component_name}')
 #         paints = root.find(f'rocket/subcomponents/stage/subcomponents/{component.tag}/appearance/paint')
-#         print(f'\t\t\tAppearance:')
+#         file.write(f'\t\t\tAppearance:')
 #         for paint in paints.items():
-#             print(f'\t\t\t\t{paint[0].capitalize()} = {paint[1]}')
-#         print(f'\t\t\t\tShine: {root.find(f"rocket/subcomponents/stage/subcomponents/{component.tag}/appearance/shine").text}')
-#         print(f'\t\t\t\tFinish: {root.find(f"rocket/subcomponents/stage/subcomponents/{component.tag}/finish").text}')
-#         print(f'\t\t\t\tMaterial: {root.find(f"rocket/subcomponents/stage/subcomponents/{component.tag}/material").text}')
-#         print(f'\t\t{new_subline}')
+#             file.write(f'\t\t\t\t{paint[0].capitalize()} = {paint[1]}')
+#         file.write(f'\t\t\t\tShine: {root.find(f"rocket/subcomponents/stage/subcomponents/{component.tag}/appearance/shine").text}')
+#         file.write(f'\t\t\t\tFinish: {root.find(f"rocket/subcomponents/stage/subcomponents/{component.tag}/finish").text}')
+#         file.write(f'\t\t\t\tMaterial: {root.find(f"rocket/subcomponents/stage/subcomponents/{component.tag}/material").text}')
+#         file.write(f'\t\t{new_subline}')
