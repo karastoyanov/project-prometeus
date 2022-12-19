3aa110043487156c3327bae0c279a1a9750530c1
Aleksandar Karastoyanov <karastoqnov.alexandar@gmail.com>
2022-12-19 09:19:39 +0200

    project update

--- prometeus/get_prometeus_data.py
+++ prometeus/get_prometeus_data.py
@@ -16,18 +16,25 @@ with open("prometeus_data.txt", "w") as file:
 	for rocket in root.findall('rocket'):
 		rocket_name = rocket.find('name').text  # Get Rocket Name
 		designer_name = rocket.find('designer').text  # Get Designer Name
+		print(f'Rocket Name: {rocket_name}\n')
+		print(f'Designer Name: {designer_name}\n')
 		file.write(f'Rocket Name: {rocket_name}\n')
 		file.write(f'Designer Name: {designer_name}\n')
 
 		# Find all Engine Configuration / 'TRUE' value point at the current active config
 		all_configs = rocket.findall('motorconfiguration')
+		print(f'{new_line}\n')
 		file.write(f'{new_line}\n')
 		for config in all_configs:
+			print(f'Engine Configuration: {str(config.attrib.values())[14:-3]}\n')
 			file.write(f'Engine Configuration: {str(config.attrib.values())[14:-3]}\n')
+		print(f'{new_line}')
 		file.write(f'{new_line}')
 
 	stages = rocket.findall('subcomponents/stage')
 	for stage in stages:
+		print(f'Number of stages: {len(stages)}\n')
+		print(f'Stage Name: {stage.find("name").text}\n')
 		file.write(f'Number of stages: {len(stages)}\n')
 		file.write(f'Stage Name: {stage.find("name").text}\n')
 
@@ -35,6 +42,7 @@ with open("prometeus_data.txt", "w") as file:
 		subcomponents = rocket.findall(f'subcomponents/stage/subcomponents/')
 
 		for subcomponent in subcomponents:
+			print(f'\tSubcomponent:\n')
 			file.write(f'\tSubcomponent:\n')
 			for attribute in subcomponent.iter():
 				if not attribute.text:
@@ -43,10 +51,12 @@ with open("prometeus_data.txt", "w") as file:
 					if "\n" not in attribute.text and attribute.text is not None:
 						if attribute.text == "Trapezoidal fin set":
 							break
+						print(f'\t\t* {attribute.tag.capitalize()} -- {attribute.text}\n')
 						file.write(f'\t\t* {attribute.tag.capitalize()} -- {attribute.text}\n')
 		
 		parts = rocket.findall(f'subcomponents/stage/subcomponents/bodytube/subcomponents')
 		for part in parts:
+			print(f'\n\t\t\tParts:\n')
 			file.write(f'\n\t\t\tParts:\n')
 			for part_name in part.iter():
 				if not part_name.text:
@@ -54,13 +64,12 @@ with open("prometeus_data.txt", "w") as file:
 				else:
 					if "\n" not in part_name.text and part_name.text is not None:
 						if part_name.tag == 'name':
+							print(f'\n')
 							file.write(f'\n')
 							part_name.tag = f'Part {part_name.tag}'
+						print(f'\t\t\t\t*{part_name.tag.capitalize()} -- {part_name.text}')
 						file.write(f'\t\t\t\t*{part_name.tag.capitalize()} -- {part_name.text}\n')
+		print(new_line)
 		file.write(new_line)
-
-					
-
-
 
 a = 5
